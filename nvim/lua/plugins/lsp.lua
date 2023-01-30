7cc4b9501e44212162bbace7e8025ca13a78cb2b
Brendan Timmons <brendan.timmons@cultureamp.com>
2023-01-30 14:09:26 +1100

    add eslint server to mason

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,6 +24,7 @@ function M.config()
     "bashls",
     "jsonls",
     "yamlls",
+    "eslint",
   }
 
   local settings = {
@@ -84,18 +85,19 @@ function M.config()
   local formatting = null_ls.builtins.formatting
 
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  -- local diagnostics = null_ls.builtins.diagnostics
+  local diagnostics = null_ls.builtins.diagnostics
 
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
   local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
   null_ls.setup({
-    debug = true,
+    debug = false,
     sources = {
       formatting.prettier.with {
         -- extra_args = { "--no-config", "--print-width 120", "--trailing-comma all", "--single-quote"}
         -- filetypes = { "javascript","typescript","css","scss","json","yaml","markdown","graphql","md","txt","html", },
       },
+      diagnostics.eslint
     },
     on_attach = function(client, bufnr)
       if client.supports_method("textDocument/formatting") then
