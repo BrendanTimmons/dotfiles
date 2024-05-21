10a6da38f0c7153b7dc43feccd64a2216d2d6416
Brendan Timmons <brendan.timmons@cultureamp.com>
2024-05-21 10:50:27 +1000

    feat: add harpoon to whichkey

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -48,7 +48,7 @@ function M.config()
 
   vim.keymap.set("n", "<S-H>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
 
-  vim.keymap.set("n", "<leader>r", function() harpoon:list():append() end)
+  vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
   vim.keymap.set("n", "<C-H>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
   vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -4,25 +4,27 @@ local M = {
 }
 
 function M.config()
+  local harpoon = require("harpoon")
+  harpoon:setup({})
   require("which-key").setup({
     {
       plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        marks = true,       -- shows a list of your marks on ' and `
+        registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
           suggestions = 20, -- how many suggestions should be shown in the list?
         },
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-          operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-          motions = false, -- adds help for motions
+          operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+          motions = false,      -- adds help for motions
           text_objects = false, -- help for text objects triggered after entering an operator
-          windows = true, -- default bindings on <c-w>
-          nav = true, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
+          windows = true,       -- default bindings on <c-w>
+          nav = true,           -- misc bindings to work with windows
+          z = true,             -- bindings for folds, spelling and others prefixed with z
+          g = true,             -- bindings for prefixed with g
         },
       },
       -- add operators that will trigger motion and text object completion
@@ -42,25 +44,25 @@ function M.config()
       },
       popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
       },
       window = {
-        border = "rounded", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "rounded",       -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
       },
       layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        height = { min = 4, max = 25 },                                             -- min and max height of the columns
+        width = { min = 20, max = 50 },                                             -- min and max width of the columns
+        spacing = 3,                                                                -- spacing between columns
+        align = "left",                                                             -- align columns left, center or right
       },
-      ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+      ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
       hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-      show_help = true, -- show help message on the command line when the popup is visible
-      triggers = "auto", -- automatically setup triggers
+      show_help = true,                                                             -- show help message on the command line when the popup is visible
+      triggers = "auto",                                                            -- automatically setup triggers
       -- triggers = {"<leader>"} -- or specify a list manually
       triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
@@ -73,12 +75,12 @@ function M.config()
   })
 
   local opts = {
-    mode = "n", -- NORMAL mode
+    mode = "n",     -- NORMAL mode
     prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true,  -- use `nowait` when creating keymaps
   }
 
   local mappings = {
@@ -87,7 +89,7 @@ function M.config()
       "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
       "Buffers",
     },
-    ["B"] = { "<cmd>Telescope bookmarks<cr>", "Bookmarks"},
+    ["B"] = { "<cmd>Telescope bookmarks<cr>", "Bookmarks" },
     ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
     ["w"] = { "<cmd>w!<CR>", "Save" },
     ["q"] = { "<cmd>q!<CR>", "Quit" },
@@ -101,11 +103,11 @@ function M.config()
 
     ["p"] = {
       name = "Goto Preview",
-      d = {"<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Definition"},
-      t = {"<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "Type Definition"},
-      i = {"<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", "Implementation"},
-      r = {"<cmd>lua require('goto-preview').goto_preview_references()<CR>", "References"},
-      x = {"<cmd>lua require('goto-preview').close_all_win()<CR>", "Close all windows"},
+      d = { "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Definition" },
+      t = { "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "Type Definition" },
+      i = { "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", "Implementation" },
+      r = { "<cmd>lua require('goto-preview').goto_preview_references()<CR>", "References" },
+      x = { "<cmd>lua require('goto-preview').close_all_win()<CR>", "Close all windows" },
     },
 
     g = {
@@ -113,6 +115,42 @@ function M.config()
       o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
       b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
       c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+    },
+
+    h = {
+      name = "Harpoon",
+      a = {
+        function() harpoon:list():add() end,
+        "Add file to harpoon list"
+      },
+      h = {
+        function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        "Toggle harpoon list"
+      },
+      ["1"] = {
+        function() harpoon:list():select(1) end,
+        "Goto file 1"
+      },
+      ["2"] = {
+        function() harpoon:list():select(2) end,
+        "Goto file 2"
+      },
+      ["3"] = {
+        function() harpoon:list():select(3) end,
+        "Goto file 3"
+      },
+      ["4"] = {
+        function() harpoon:list():select(4) end,
+        "Goto file 4"
+      },
+      ["["] = {
+        function() harpoon:list():prev() end,
+        "Goto previous file"
+      },
+      ["]"] = {
+        function() harpoon:list():nexe() end,
+        "Goto previous file"
+      }
     },
 
     l = {
@@ -140,7 +178,7 @@ function M.config()
     },
     r = {
       name = "Ruby",
-      t = {"<cmd>TermExec direction=float cmd='bundle exec rspec %'<cr>", "Test File"},
+      t = { "<cmd>TermExec direction=float cmd='bundle exec rspec %'<cr>", "Test File" },
     },
     s = {
       name = "Search",
@@ -169,22 +207,22 @@ function M.config()
     },
     x = {
       name = "Trouble",
-      x = {"<cmd>TroubleToggle<cr>", "Toggle Trouble"},
-      w = {"<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics"},
-      d = {"<cmd>TroubleToggle document_diagnostics<cr>", "Document Diagnostics"},
-      l = {"<cmd>TroubleToggle loclist<cr>", "Location List"},
-      q = {"<cmd>TroubleToggle quickfix<cr>", "Quickfix"},
-      r = {"<cmd>TroubleToggle lsp_references<cr>", "LSP References"},
+      x = { "<cmd>TroubleToggle<cr>", "Toggle Trouble" },
+      w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics" },
+      d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document Diagnostics" },
+      l = { "<cmd>TroubleToggle loclist<cr>", "Location List" },
+      q = { "<cmd>TroubleToggle quickfix<cr>", "Quickfix" },
+      r = { "<cmd>TroubleToggle lsp_references<cr>", "LSP References" },
     }
   }
 
   local vopts = {
-    mode = "v", -- VISUAL mode
+    mode = "v",     -- VISUAL mode
     prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true,  -- use `nowait` when creating keymaps
   }
   local vmappings = {
     ["/"] = { "<ESC><CMD>lua require(\"Comment.api\").toggle_linewise_op(vim.fn.visualmode())<CR>", "Comment" },
