279d28e599e6b6e3f779dce465adb22f28ef1a4b
Brendan Timmons <brendan.timmons@cultureamp.com>
2025-05-16 13:07:08 +1000

    kill project

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ local M = {
   "nvim-telescope/telescope.nvim",
   cmd = { "Telescope" },
   dependencies = {
-    {"nvim-lua/plenary.nvim"},
+    { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   }
 }
@@ -94,16 +94,15 @@ function M.config()
     },
     extensions = {
       fzf = {
-        fuzzy = true,                    -- false will only do exact matching
-        override_generic_sorter = true,  -- override the generic sorter
-        override_file_sorter = true,     -- override the file sorter
-        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+        fuzzy = true,                   -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
       },
     },
   })
 
-  telescope.load_extension('projects')
   telescope.load_extension('fzf')
 
   -- local builtin = require('telescope.builtin')
