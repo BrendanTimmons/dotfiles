b7c15a247d8a9aa4485676ef231da5aeabe29a8c
Brendan Timmons <brendan.timmons@cultureamp.com>
2026-04-15 11:39:54 +1000

    fix all my broken shit

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,24 +1,25 @@
 local M = {
-  'stevearc/oil.nvim',
-  opts = {
-    float = {
-      padding = 5,
-      max_width = 0.8
-    },
-    view_options = {
-      show_hidden = true
-    },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<leader>o", "<cmd>Oil --float --preview<CR>", desc = "Oil float" },
-    { "<leader>O", "<cmd>Oil --preview<CR>",         desc = "Oil" },
-  },
-  lazy = false,
+	"stevearc/oil.nvim",
+	opts = {
+		float = {
+			padding = 5,
+			max_width = 0.8,
+			border = "double",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<leader>o", "<cmd>Oil --float --preview<CR>", desc = "Oil float" },
+		{ "<leader>O", "<cmd>Oil --preview<CR>", desc = "Oil" },
+	},
+	lazy = false,
 }
 
 function M.config()
-  require("oil").setup(M.opts)
+	require("oil").setup(M.opts)
 end
 
 return M
