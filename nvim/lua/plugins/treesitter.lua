07a5e524550f79392bcf956e0064ac82c462579d
Brendan Timmons <brendan.timmons@cultureamp.com>
2026-04-15 10:57:59 +1000

    fix: treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,72 @@
-local M = {
+return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
   lazy = false,
   build = ":TSUpdate",
+  config = function()
+    local ts = require 'nvim-treesitter'
+    local parsers = {
+      "bash",
+      "comment",
+      "css",
+      "diff",
+      "dockerfile",
+      "elixir",
+      "git_config",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "heex",
+      "hcl",
+      "html",
+      "http",
+      "java",
+      "javascript",
+      "jsdoc",
+      "json",
+      "json5",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "rst",
+      "rust",
+      "scss",
+      "ssh_config",
+      "sql",
+      "terraform",
+      "typst",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    }
+
+    for _, parser in ipairs(parsers) do
+      ts.install(parser)
+    end
+
+    -- Not every tree-sitter parser is the same as the file type detected
+    -- So the patterns need to be registered more cleverly
+    local patterns = {}
+    for _, parser in ipairs(parsers) do
+      local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+      for _, pp in pairs(parser_patterns) do
+        table.insert(patterns, pp)
+      end
+    end
+
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = patterns,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
-
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = false,
-    },
-  })
-end
-
-return M
